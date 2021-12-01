2b4442deb619462257fab85c4560a1ea5976ee19
Sensei <51503516+ashutosh1615@users.noreply.github.com>
2021-12-01 22:31:23 +0530

    Delete backend/ackley directory

--- backend/ackley/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Contacts,Bookings
-admin.site.register(Contacts)
-admin.site.register(Bookings)
-
--- backend/ackley/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AckleyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ackley'
